4f6b4c0d4e9d1b178fe7b4082ff52c16269894fe
Gabriel <112896989+Gstein77@users.noreply.github.com>
2022-11-20 10:35:47 -0300

    Update User.java

--- User/User.java
+++ User/User.java
@@ -2,7 +2,14 @@ public abstract class User {
     public String nome;
     public String endereco;
     public int idade;
+    
+    public Client(String nome, String endereco, int idade) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.idade = idade;
 
+    }
+    
     protected abstract void acessarCardapio();
 
 }
