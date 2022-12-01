12296d6042158fa872e17ed6373d749781ed614c
Gabriel <112896989+Gstein77@users.noreply.github.com>
2022-11-30 21:44:43 -0300

    Add files via upload

--- Main/MainLogin.java
+++ Main/MainLogin.java
@@ -8,17 +8,18 @@ import Login.Login;
 public class MainLogin {
     public static boolean fazerLogin(Scanner ler, Login login) throws IOException{
         System.out.println("Digite o login:");
-        String l = ler.nextLine();
+        String l = ler.next();
+        
         System.out.println("Digite a senha:");
-        String s = ler.nextLine();
+        String s = ler.next();
         return login.logar(l, s);
     }
 
     public static boolean cadastrarLogin(Scanner ler, Login login) throws IOException{
         System.out.println("Digite o login:");
-        String l = ler.nextLine();
+        String l = ler.next();
         System.out.println("Digite a senha:");
-        String s = ler.nextLine();
+        String s = ler.next();
         return login.cadastrar(l, s);
     }
 }
